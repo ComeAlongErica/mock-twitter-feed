ac1f60d4985970f15d9be427d157deaba7f30763
ComeAlongErica <ericaraephoto@gmail.com>
2018-11-10 16:52:54 -0500

    remove comments and console.log

--- app/components/children/post-form/expand.js
+++ app/components/children/post-form/expand.js
@@ -5,19 +5,13 @@ function expandOnFocus() {
     restrict: "A",
     link: function($scope, $element, $attrs) {
       $element.on("focus", () => {
-        // If the element's sibling is a paragraph...
         if ($element[0].tagName === "INPUT" || $element[0].tagName === "TEXTAREA") {
-          // The element's sibling now has a green color
           $element[0].style.padding = "5px 0";
-          console.log("focused");
         }
       });
       $element.on("blur", () => {
-        // If the element's sibling is a paragraph...
         if ($element[0].tagName === "INPUT" || $element[0].tagName === "TEXTAREA") {
-          // The element's sibling now has a green color
           $element[0].style.padding = "";
-          console.log("focused");
         }
       });
     }
